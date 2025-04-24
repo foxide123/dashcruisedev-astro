f8f12862b684b51fb8420045df889def375eb31a
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-24 16:19:10 +0200

    Init wih basic hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,35 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  i18n: {
+    locales: ["en", "de"],
+    defaultLocale: "en",
+    fallback: {
+      de: "en"
+    },
+    routing: {
+      prefixDefaultLocale: true,
+      fallbackType: "rewrite"
+    }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": new URL("./src", import.meta.url).pathname
+      }
+    },
+
+    plugins: [tailwindcss()]
+  },
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
+  /* adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  }),
+  }), */
 });
---
+++ src/data/MenuItemsData.ts
@@ -0,0 +1,30 @@
+export const MenuItemsData = [
+    {
+        name: "Home",
+        pathname: "/"
+    },
+    {
+        name: "About",
+        pathname: "/about"
+    },
+    {
+        name: "Contact",
+        pathname: "/contact"
+    },
+    {
+        name: "Blog",
+        pathname: "/blog"
+    },
+   /*  {
+        name: "Pages",
+        pathname: "/pages",
+        nested: [
+            {
+                pathname: "/page1"
+            },
+            {
+                pathname: "/page2"
+            }
+        ]
+    } */
+]
