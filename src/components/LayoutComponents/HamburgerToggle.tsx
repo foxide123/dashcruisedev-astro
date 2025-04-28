8a875e691c54667739cbe69b356c6b2838d13358
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-28 16:01:56 +0200

    Replacing HamburgerToggle from astro to tsx

---
+++ src/components/LayoutComponents/HamburgerToggle.tsx
@@ -0,0 +1,27 @@
+// HamburgerToggle.tsx
+import { useState } from 'react';
+import HamburgerIconCustom from '@/components/icons/HamburgerIconCustom';
+
+export default function HamburgerToggle() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    const menu = document.getElementById('menu');
+    if (!menu) return;
+
+    if (!isOpen) {
+      menu.classList.add('max-h-[500px]', 'opacity-100');
+      menu.classList.remove('max-h-0', 'opacity-0');
+    } else {
+      menu.classList.remove('max-h-[500px]', 'opacity-100');
+      menu.classList.add('max-h-0', 'opacity-0');
+    }
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <button onClick={handleToggle} className="cursor-pointer">
+      <HamburgerIconCustom stroke={3} width={50} height={50} />
+    </button>
+  );
+}
