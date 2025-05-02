eb69902774d57b805d626fcb9f5e9f3a119b4f4d
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-05-02 14:59:46 +0200

    Udpating article data types and hreflang generation

--- src/types/article_types.tsx
+++ src/types/article_types.tsx
@@ -1,21 +1,23 @@
 export type SlugWithLocale = {
-    slug: string;
-    locale: string;
-  };
-  
-  export type ArticleData = {
-    id: "string";
-    article_id: "string";
-    locale: number;
-    title: "string";
-    slug: "string";
-    body: "string";
-    introduction: "string";
-  };
-  
-  export type SectionData = {
-    section_slug: string;
-    section_title: string;
-    order: number;
-    ArticleTranslation: any;
-  }
+  translation_id: string;
+  article_id: string;
+  slug: string;
+  locale: string;
+};
+
+export type TranslationData = {
+  id: 'string';
+  article_id: 'string';
+  locale: number;
+  title: 'string';
+  slug: 'string';
+  body: 'string';
+  introduction: 'string';
+};
+
+export type SectionData = {
+  section_slug: string;
+  section_title: string;
+  order: number;
+  ArticleTranslation: any;
+};
