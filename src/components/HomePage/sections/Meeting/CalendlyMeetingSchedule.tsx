1209aac5a0984387958e7d9eb5bacb22407428d9
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-05-22 23:55:15 +0200

    Optimizing performance

--- src/components/HomePage/sections/Meeting/CalendlyMeetingSchedule.tsx
+++ src/components/HomePage/sections/Meeting/CalendlyMeetingSchedule.tsx
@@ -6,18 +6,26 @@ export default function CalendlyMeetingSchedule() {
 
   const options = {
     root: null,
-    rootMargin: '0px 0px -10% 0px',
-    threshold: 0.1
+    rootMargin: '0px 0px 100% 0px',
   };
 
   useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) {
-        setShowWidget(true);
+    const loadCalendly = () => {
+      if (
+        !document.querySelector(
+          'script[src="https://assets.calendly.com/assets/external/widget.js"]'
+        )
+      ) {
         const script = document.createElement('script');
         script.src = 'https://assets.calendly.com/assets/external/widget.js';
         script.async = true;
         document.body.appendChild(script);
+      }
+      setShowWidget(true);
+    };
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        loadCalendly();
       }
     }, options);
     if (containerRef.current) observer.observe(containerRef.current);
@@ -28,7 +36,10 @@ export default function CalendlyMeetingSchedule() {
   }, [containerRef, options]);
 
   return (
-    <div ref={containerRef} className="w-full min-h-[700px]">
+    <div
+      ref={containerRef}
+      className="w-full min-h-[700px] bg-[#F7F7F7] rounded-[12px]"
+    >
       {showWidget && (
         <div
           className="w-full calendly-inline-widget min-w-[320px] h-[700px]"
