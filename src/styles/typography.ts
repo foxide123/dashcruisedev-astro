16adc15f0ec4b95967602efbc73c92c591b3b08c
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-05-28 22:35:55 +0200

    Creating reusable typography and updating UI

---
+++ src/styles/typography.ts
@@ -0,0 +1,21 @@
+export const mobileTypography = {
+    H1: "mobile-H1",
+    H2Regular: "mobile-H2-Regular",
+    H2Semibold: "mobile-H2-Semibold",
+    H4: "mobile-H4",
+    Body18: "mobile-Body18",
+    Paragraph: "mobile-Paragraph",
+}
+
+export const desktopTypography = {
+    H1: "desktop-H1",
+    H2: "desktop-H2",
+    H2Regular: "desktop-H2-Regular",
+    H3Semibold: "desktop-H3-Semibold",
+    H3Regular: "desktop-H3-Regular",
+    H4: "desktop-H4",
+    Body28: "desktop-Body-28",
+    Body24: "desktop-Body-24",
+    Menu: "desktop-Menu",
+    Paragraph: "desktop-Paragraph"
+}
